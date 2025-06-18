2b45c9eb1ebacb9dfb1469bb3b6cc0c47a0689b2
Ariel Gonçalves <yarizxnnfa@gmail.com>
2025-06-18 14:37:15 -0300

    Função extra adicionada em dica.py para exibir dica na tela durante o jogo

---
+++ Recursos/dica.py
@@ -0,0 +1,8 @@
+import pygame
+
+def mostrar_dica(tela, largura, altura):
+    fonte_menor = pygame.font.Font("Recursos/letra.ttf", 20)
+    mensagem = "Dica: Mantenha-se em movimento para sobreviver!"
+    texto = fonte_menor.render(mensagem, True, (255, 255, 0))  
+    tela.blit(texto, (largura // 2 - texto.get_width() // 2, altura - 40))
+
